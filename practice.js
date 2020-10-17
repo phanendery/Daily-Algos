169adccd4522eae3eaca9ffb1bed3b9a3a7b61b5
phanendery <phanendery@gmail.com>
2020-10-17 14:42:04 -0700

    bSearch todays algo

--- practice.js
+++ practice.js
@@ -1,9 +1,29 @@
-function twoNumberSum(array,targetSum){
+const binarySearch = (array, target) => {
+  let startIndex = 0;
+  let endIndex = array.length - 1;
+  while(startIndex <= endIndex) {
+    let middleIndex = Math.floor((startIndex + endIndex) / 2);
+    if(target === array[middleIndex) {
+      return console.log("Target was found at index " + middleIndex);
+    }
+    if(target > array[middleIndex]) {
+      console.log("Searching the right side of Array")
+      startIndex = middleIndex + 1;
+    }
+    if(target < array[middleIndex]) {
+      console.log("Searching the left side of array")
+      endIndex = middleIndex - 1;
+    }
+    else {
+      console.log("Not Found this loop iteration. Looping another iteration.")
+    }
+  }
   
+  console.log("Target value not found in array");
 }
 
 
 let array = [3,5,4,8,11,1,-1,6];
 targetSum = 10;
 
-console.log(twoNumberSum(array,targetSum));
+console.log(binarySearch(array,targetSum));
