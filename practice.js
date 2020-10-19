eb6fe6c2438f2d68d4bbb74da114ee87f699e2ce
phanendery <phanendery@gmail.com>
2020-10-19 15:31:32 -0700

    3 diff algos today

--- practice.js
+++ practice.js
@@ -1,30 +1,10 @@
-const binarySearch = (array, target) => {
-  let startIndex = 0;
-  let endIndex = array.length - 1;
-  while(startIndex <= endIndex) {
-    let middleIndex = Math.floor((startIndex + endIndex) / 2);
-    if(target === array[middleIndex]) {
-      return console.log("Target was found at index " + middleIndex);
-    }
-    if(target > array[middleIndex]) {
-      console.log("Searching the right side of Array")
-      startIndex = middleIndex + 1;
-    }
-    if(target < array[middleIndex]) {
-      console.log("Searching the left side of array")
-      endIndex = middleIndex - 1;
-    }
-    else {
-      console.log("Not Found this loop iteration. Looping another iteration.")
-    }
-  }
-  
-  console.log("Target value not found in array");
+function test(string, key) {
+  const alpha = "abcdefghijklmnopqrstuvwxyz".split("");
+  console.log(alpha);
+  //key and string values and add em accordingly 
 }
 
+let string = "abc";
+const key = 3;
 
-
-let array = [3,];
-targetSum = 5;
-
-console.log(binarySearch(array,targetSum));
+console.log(test(string, key));
